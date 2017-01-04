41b0c7b8d1df94e0a8771b1270e1f2cbb3986038
yjxiong <yjxiong@users.noreply.github.com>
2017-01-04 23:01:03 +0800

    Fix CPU Flow Image Encoding

--- src/dense_flow.cpp
+++ src/dense_flow.cpp
@@ -40,7 +40,7 @@ void calcDenseFlow(string file_name, int bound, int type, int step,
 
             vector<uchar> str_x, str_y, str_img;
             split(flow, flow_split);
-            encodeFlowMap(flow_split[0], flow_split[0], str_x, str_y, bound);
+            encodeFlowMap(flow_split[0], flow_split[1], str_x, str_y, bound);
             imencode(".jpg", capture_image, str_img);
 
             output_x.push_back(str_x);
