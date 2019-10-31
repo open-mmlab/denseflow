57567ddcaed6ecdf19cd3e582f0efb52c61951ed
lizz <lizz@sensetime.com>
2019-10-31 14:47:19 +0800

    nvflow
    Signed-off-by: lizz <lizz@sensetime.com>

---
+++ b.sh
@@ -0,0 +1,6 @@
+cmake \
+    -DOpenCV_DIR=/home/lizz/app \
+    -DLIBZIP_LIBRARY=/home/lizz/app/lib64/libzip.so \
+    -DLIBZIP_INCLUDE_DIR_ZIP=/home/lizz/app/include \
+    -DLIBZIP_INCLUDE_DIR_ZIPCONF=/home/lizz/app/include \
+    ..
